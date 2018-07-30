95d741ea86f30d5177535218db66f3b3d225093a
Kyle Lee <kvlee14@gmail.com>
2018-07-30 13:36:50 +0800

    First commit

---
+++ src/App.java
@@ -0,0 +1,10 @@
+public class App {
+
+    public static void main(String args[]){
+        System.out.print("My first committed app");
+    }
+
+    public String sample(){
+        return "Test";
+    }
+}
