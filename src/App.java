90e6e0e075b566784f5867deca75237ed77edac7
Kyle Lee <kvlee14@gmail.com>
2018-07-30 14:00:12 +0800

    Second commit

--- src/App.java
+++ src/App.java
@@ -2,6 +2,7 @@ public class App {
 
     public static void main(String args[]){
         System.out.print("My first committed app");
+        System.out.print("Another line to commit");
     }
 
     public String sample(){
